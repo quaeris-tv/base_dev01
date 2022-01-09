d42128fab3131d4f9d916b3f95220f41ea9d39db
Malebestia <malebestia@yahoo.it>
2022-01-09 12:49:30 +0100

    .

---
+++ laravel/config/local/test/xra.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+ return [
+     'adm_theme' => 'AdminLTE',
+     //'adm_theme' => 'SbAdmin2Bs4',
+     'enable_ads' => '1',
+     'main_module' => 'xot',
+     'primary_lang' => 'it',
+     //'pub_theme' => 'DirectoryBs5',
+     'pub_theme' => 'LaravelIo',
+ ];
