1dd8ebf3ef5eaa4b5e2b527b47a02fb65c660e96
marco xot <marco.sottana@gmail.com>
2022-10-21 12:21:11 +0200

    .

--- bashscripts_old/git_branch.sh
+++
@@ -1,9 +0,0 @@
-git config --global core.autocrlf true
-git config --global core.safecrlf false
-git config --global submodule.recurse true
-git config --global core.fileMode false
-git branch --set-upstream-to=origin/master master
-git submodule foreach git branch --set-upstream-to=origin/master master
-git push --set-upstream origin master
-git submodule foreach git push --set-upstream origin master
-read -p "Press [Enter] key to exit..."
---
+++ public_html/themes/BsItalia/dist/js/app.js
@@ -0,0 +1,2 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[773],{1459:(e,o,s)=>{s(5050)},5050:(e,o,s)=>{s(9755);window._=s(6486);try{window.$=s(9755),s(8824)}catch(e){console.log(e)}window.axios=s(9669),window.axios.defaults.headers.common["X-Requested-With"]="XMLHttpRequest"},3907:()=>{}},e=>{var o=o=>e(e.s=o);e.O(0,[170,898],(()=>(o(1459),o(3907))));e.O()}]);
+//# sourceMappingURL=app.js.map
