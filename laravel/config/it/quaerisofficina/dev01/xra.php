9ab6429001163c05b14eb4b398d63c28f76b811b
marco xot <marco.sottana@gmail.com>
2022-12-05 12:40:18 +0100

    up

--- laravel/config/it/quaerisofficina/dev01/xra.php
+++ laravel/config/it/quaerisofficina/dev01/xra.php
@@ -9,17 +9,17 @@ declare(strict_types=1);
      'pub_theme' => 'BsItalia',
     //  'pub_theme' => 'DirectoryBs5',
      'main_module'=> 'Quaeris',
-     'home' => 'template.homepage',
+    //  'home' => 'template.homepage',
     // 'home' => 'template.amministrazione'
     //  'home' => 'segnalazione-disservizio.trovare_informazioni_servizio',
     //  'home' => 'segnalazione-disservizio.acconsentire_trattamento_dati_inseriti',
-    // 'home' => 'segnalazione-disservizio.inserire_dati_specifici'
+    'home' => 'segnalazione-disservizio.inserire_dati_specifici'
     // 'home' => 'segnalazione-disservizio.conferma_riepilogo_informazioni_inserite'
-    // 'home' => 'segnalazione-disservizio.trovare_informazioni_servizio',
 
 
 
-     //  'home' => 'segnalazione-disservizio.tutte_segnalazioni_trovare_informazioni'
+
+    //   'home' => 'segnalazione-disservizio.tutte_segnalazioni_trovare_informazioni'
     
     
  ];
