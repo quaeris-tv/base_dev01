3df432dc5bb424f61bb685823155603409b08036
marco xot <marco.sottana@gmail.com>
2022-12-29 12:38:41 +0100

    up

--- laravel/config/local/dev01/xra.php
+++ laravel/config/local/dev01/xra.php
@@ -12,13 +12,5 @@ declare(strict_types=1);
      'adm_home' => '01',
      'search_action' => 'it/videos',
      'show_trans_key' => false,
-    'adm_theme' => 'AdminLTE',
-    // 'adm_theme' => 'SbAdmin2Bs4',
-    'enable_ads' => '1',
-    'main_module' => 'xot',
-    'primary_lang' => 'it',
-    'pub_theme' => 'DirectoryBs5',
-    'adm_home' => '01',
-    'search_action' => 'it/videos',
-    'show_trans_key' => false,
+    
 ];
