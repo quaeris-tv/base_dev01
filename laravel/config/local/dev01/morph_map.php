3aef702fe9e7574639e2d5f76a4e18612e2006f7
marco76tv <marco.sottana@gmail.com>
2023-01-24 19:41:24 +0100

    .

--- laravel/config/local/dev01/morph_map.php
+++ laravel/config/local/dev01/morph_map.php
@@ -1,7 +1,10 @@
-<?php return array (
+<?php
+
+ return array (
   'customer' => 'Modules\Quaeris\Models\Customer',
   'home' => 'Modules\Xot\Models\Home',
   'mixed_chart' => 'Modules\Chart\Models\MixedChart',
   'profile' => 'Modules\Quaeris\Models\Profile',
   'user' => 'Modules\LU\Models\User',
+  'ticket' => 'Modules\Ticket\Models\Ticket',
 );
---
+++ laravel/config/local/dev01/ticket.php
@@ -0,0 +1,10 @@
+<?php
+
+
+return [
+    'categories'=>[
+        'video-corrotto',
+        'fuori-sync',
+        'altro',
+    ],
+];
