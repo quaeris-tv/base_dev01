5c6c25466ba5b822132ab023812233921d735f74
marco xot <marco.sottana@gmail.com>
2023-01-28 11:48:41 +0100

    .

--- laravel/config/it/quaerisofficina/dev01/xra.php
+++ laravel/config/it/quaerisofficina/dev01/xra.php
@@ -6,11 +6,16 @@ return [
     'adm_home' => '03',
     'enable_ads' => '1',
     'primary_lang' => 'it',
+<<<<<<< HEAD
     'pub_theme' => 'KlekooPub',
+=======
+    'pub_theme' => 'BsItalia',
+>>>>>>> faa1d54 (up)
     // 'pub_theme' => 'DirectoryBs5',
     'main_module'=> 'Quaeris',
     // 'home' => 'template.homepage',
     // 'home' => 'template.amministrazione'
+<<<<<<< HEAD
 <<<<<<< HEAD
     // 'home' => 'segnalazione-disservizio.trovare_informazioni_servizio',
     // 'home' => 'segnalazione-disservizio.acconsentire_trattamento_dati_inseriti',
@@ -24,6 +29,11 @@ return [
     // 'home' => 'segnalazione-disservizio.inserire_dati_specifici'
 
 >>>>>>> 86b8b39 (up)
+=======
+    // 'home' => 'segnalazione-disservizio.trovare_informazioni_servizio',
+    // 'home' => 'segnalazione-disservizio.acconsentire_trattamento_dati_inseriti',
+    // 'home' => 'segnalazione-disservizio.inserire_dati_specifici'
+>>>>>>> faa1d54 (up)
     // 'home' => 'segnalazione-disservizio.conferma_riepilogo_informazioni_inserite'
     // 'home' => 'segnalazione-disservizio.tutte_segnalazioni_trovare_informazioni'
 ];
