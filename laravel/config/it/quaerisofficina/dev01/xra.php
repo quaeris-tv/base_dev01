faa1d540c6378da442fa5c2d2327924f667b6da5
marco xot <marco.sottana@gmail.com>
2023-01-28 11:48:41 +0100

    up

--- laravel/config/it/quaerisofficina/dev01/xra.php
+++ laravel/config/it/quaerisofficina/dev01/xra.php
@@ -1,50 +1,19 @@
 <?php
 
-
-
 declare(strict_types=1);
-
-
-
- return [
-
-     'adm_theme' => 'KlekooAdm',
-     'adm_home' => '03',
-     'enable_ads' => '1',
-
-     'primary_lang' => 'it',
-
-     'pub_theme' => 'BsItalia',
-
-    //  'pub_theme' => 'DirectoryBs5',
-
-     'main_module'=> 'Quaeris',
-
-    //  'home' => 'template.homepage',
-
+return [
+    'adm_theme' => 'KlekooAdm',
+    'adm_home' => '03',
+    'enable_ads' => '1',
+    'primary_lang' => 'it',
+    'pub_theme' => 'BsItalia',
+    // 'pub_theme' => 'DirectoryBs5',
+    'main_module'=> 'Quaeris',
+    // 'home' => 'template.homepage',
     // 'home' => 'template.amministrazione'
-
-    //  'home' => 'segnalazione-disservizio.trovare_informazioni_servizio',
-
-    //  'home' => 'segnalazione-disservizio.acconsentire_trattamento_dati_inseriti',
-
+    // 'home' => 'segnalazione-disservizio.trovare_informazioni_servizio',
+    // 'home' => 'segnalazione-disservizio.acconsentire_trattamento_dati_inseriti',
     // 'home' => 'segnalazione-disservizio.inserire_dati_specifici'
-
     // 'home' => 'segnalazione-disservizio.conferma_riepilogo_informazioni_inserite'
-
-
-
-
-
-
-
-
-
-    //   'home' => 'segnalazione-disservizio.tutte_segnalazioni_trovare_informazioni'
-
-    
-
-    
-
- ];
-
+    // 'home' => 'segnalazione-disservizio.tutte_segnalazioni_trovare_informazioni'
+];
