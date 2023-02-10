3184a95f9848cf53fa425d010ea8a9d3f78e9f45
marco xot <marco.sottana@gmail.com>
2023-02-10 10:51:15 +0100

    rebase

--- laravel/config/it/quaerisofficina/dev01b/xra.php
+++ laravel/config/it/quaerisofficina/dev01b/xra.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
  return [
      'adm_theme' => 'AdminLTE',
+<<<<<<< HEAD
+=======
+     'adm_home' => '01',
+>>>>>>> 13e9c969 (up)
      'enable_ads' => '1',
      'primary_lang' => 'it',
-     'pub_theme' => 'KlekooPub',
+     'pub_theme' => 'BsItalia',
      'main_module'=> 'Quaeris',
      //'home' => 'segnalazione-disservizio.inserire_dati_specifici'
+     'show_trans_key' => false,
     
  ];
