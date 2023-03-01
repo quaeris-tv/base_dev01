192db130e0fd50bd848d947395626e58f30b0b62
marco76tv <marco.sottana@gmail.com>
2023-03-01 15:21:43 +0100

    up

--- laravel/config/it/quaerisofficina/dev01/xra.php
+++ laravel/config/it/quaerisofficina/dev01/xra.php
@@ -3,13 +3,17 @@
 declare(strict_types=1);
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> f34f4d2 (rebase)
+=======
+>>>>>>> 9ab9892 (up)
 return [
     'adm_theme' => 'KlekooAdm',
     'adm_home' => '03',
     'enable_ads' => '1',
     'primary_lang' => 'it',
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
     'pub_theme' => 'KlekooPub',
@@ -60,20 +64,28 @@ return [
     //  'home' => 'template.homepage',
 
 =======
+=======
+>>>>>>> 9ab9892 (up)
     'pub_theme' => 'KlekooPub',
     // 'pub_theme' => 'DirectoryBs5',
     'main_module'=> 'Quaeris',
     // 'home' => 'template.homepage',
+<<<<<<< HEAD
 >>>>>>> f34f4d2 (rebase)
+=======
+>>>>>>> 9ab9892 (up)
     // 'home' => 'template.amministrazione'
     // 'home' => 'segnalazione-disservizio.trovare_informazioni_servizio',
     // 'home' => 'segnalazione-disservizio.acconsentire_trattamento_dati_inseriti',
     // 'home' => 'segnalazione-disservizio.inserire_dati_specifici'
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 >>>>>>> 344b7a8 (.)
 =======
 >>>>>>> f34f4d2 (rebase)
+=======
+>>>>>>> 9ab9892 (up)
     // 'home' => 'segnalazione-disservizio.conferma_riepilogo_informazioni_inserite'
     // 'home' => 'segnalazione-disservizio.tutte_segnalazioni_trovare_informazioni'
 ];
